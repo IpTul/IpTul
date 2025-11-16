0a70d5f5eaf0c6efdbd0f2cb15a7e99b6b847bae
IpTul <lthfrhmn21@gmail.com>
2025-11-16 18:08:50 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+// import { useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Toaster } from "@/components/ui/toaster";
+import { Home } from "./pages/Home";
+import { NotFound } from "./pages/NotFound";
+
+function App() {
+	// const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<Toaster />
+			<BrowserRouter>
+				<Routes>
+					<Route index element={<Home />} />
+					<Route path="*" element={<NotFound />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
