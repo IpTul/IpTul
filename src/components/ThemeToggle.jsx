34b481be88c3779ff4b343f8a4fc9537dcf7ef50
IpTul <lthfrhmn21@gmail.com>
2025-11-16 18:20:43 +0800

    2

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -32,11 +32,12 @@ export const ThemeToggle = () => {
 		<button
 			onClick={toggleTheme}
 			className={cn(
-				"fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-3",
-				"focus:outlin-hidden",
+				"fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300",
+				"focus:outline-none",
+				"hover:bg-gray-200 dark:hover:bg-gray-800"
 			)}
+			aria-label="Toggle theme"
 		>
-			{" "}
 			{isDarkMode ? (
 				<Sun className="h-6 w-6 text-yellow-300" />
 			) : (
